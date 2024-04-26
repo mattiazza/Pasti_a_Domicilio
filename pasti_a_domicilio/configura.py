c7171ee56860291a1dd17d7d070d7401c891a984
Mattia Piazza <mattiapiazza@gmail.com>
2024-04-26 16:47:45 +0200

    First commit

--- pasti_a_domicilio/configura.py
+++ pasti_a_domicilio/configura.py
@@ -1,4 +1,12 @@
+import os
 import numpy as np
+import pandas as pd
 
-for i in range(10):
-    print(i)
+print(os.getcwd())
+os.chdir('/examples')
+print(os.getcwd())
+df = pd.read_excel('/examples/example_table.xlsx')
+
+
+# Print the first few rows of the DataFrame
+print(df.head())
